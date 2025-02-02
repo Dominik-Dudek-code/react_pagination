92fdc1cd224273924ab97d9e58fabf6e47fc0887
Dominik Dudek <dom-dud774@o2.pl>
2025-02-03 00:32:00 +0100

    Task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,82 @@
-export const Pagination = () => {};
+import { getNumbers } from '../../utils';
+
+type PaginationProps = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<PaginationProps> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const maxPagination = Math.ceil(total / perPage);
+  const paginations = getNumbers(1, maxPagination);
+
+  return (
+    <ul className="pagination">
+      <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={event => {
+            if (currentPage === 1) {
+              event.preventDefault();
+
+              return;
+            }
+
+            onPageChange(currentPage - 1);
+          }}
+        >
+          «
+        </a>
+      </li>
+
+      {paginations.map(pagination => {
+        return (
+          <li
+            className={`page-item ${pagination === currentPage ? 'active' : ''}`}
+            key={pagination}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${pagination}`}
+              onClick={() => onPageChange(pagination)}
+            >
+              {pagination}
+            </a>
+          </li>
+        );
+      })}
+
+      <li
+        className={`page-item ${currentPage === maxPagination ? 'disabled' : ''}`}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === maxPagination}
+          onClick={event => {
+            if (currentPage === maxPagination) {
+              event.preventDefault();
+
+              return;
+            }
+
+            onPageChange(currentPage + 1);
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
